5483c65fcfa5d697430d2fdca59c3eaa73526bc5
[dyad] <pranto48@gmail.com>
2025-10-28 12:49:37 +0600

    [dyad] Fixed undefined function error in auth_check.php by including config.php. - wrote 1 file(s) + extra files edited outside of Dyad

--- portal.itsupport.com.bd/docker-ampnm/includes/auth_check.php
+++ portal.itsupport.com.bd/docker-ampnm/includes/auth_check.php
@@ -1,6 +1,8 @@
 <?php
 // Include the main bootstrap file which handles DB checks and starts the session.
 require_once __DIR__ . '/bootstrap.php';
+// Include config.php to make license-related functions available
+require_once __DIR__ . '/../config.php';
 
 // If the user is not logged in, redirect to the login page.
 if (!isset($_SESSION['user_id'])) {
